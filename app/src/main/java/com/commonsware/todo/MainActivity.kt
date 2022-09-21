e99df4cc51012e5061da1d284f3c80c52467cf1d
hejl <hjlarry@163.com>
2022-09-21 15:11:50 +0800

    add about activity

--- app/src/main/java/com/commonsware/todo/MainActivity.kt
+++ app/src/main/java/com/commonsware/todo/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.commonsware.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.commonsware.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.actions, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+        R.id.about ->{
+            startActivity(Intent(this, AboutActivity::class.java))
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
