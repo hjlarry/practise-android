551083c38d3788d4df36773f74b6ca9318dbd771
hejl <hjlarry@163.com>
2022-09-28 10:53:14 +0800

    add fragment basic

---
+++ MyFragment/app/src/main/java/com/example/myfragment/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.myfragment
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<View>(R.id.button)
+        btn.setOnClickListener{
+            replaceFragment(AnotherRightFragment())
+        }
+        replaceFragment(RightFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.rightLayout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
+
+class LeftFragment: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.left_fragment, container, false)
+    }
+}
+
+class RightFragment: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.right_fragment, container, false)
+    }
+}
+
+class AnotherRightFragment: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.an_right_fragment, container, false)
+    }
+}
