1f8b39b55ddb8b73da3f03b09c4f86e778d22410
hejl <hjlarry@163.com>
2022-10-01 14:13:40 +0800

    add send custom broadcast

--- BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.kt
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.kt
@@ -4,8 +4,9 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat. app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,13 @@ class MainActivity : AppCompatActivity() {
         intentFilter.addAction("android.intent.action.TIME_TICK")
         timeChangeReceiver = TimeChangeReceiver()
         registerReceiver(timeChangeReceiver, intentFilter)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            val intent = Intent("com.example.broadcasttest.MY_BROADCAST")
+            intent.setPackage(packageName)
+            sendBroadcast(intent)
+        }
     }
 
     override fun onDestroy() {
@@ -28,8 +36,14 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-class TimeChangeReceiver: BroadcastReceiver(){
+class TimeChangeReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Toast.makeText(context, "Some thing", Toast.LENGTH_SHORT).show()
+    }
+}
+
+class MyBroadcastReciver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, "received my broadcast", Toast.LENGTH_LONG).show()
     }
 }
