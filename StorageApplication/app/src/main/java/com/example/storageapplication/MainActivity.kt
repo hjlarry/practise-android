fab8bdc54504233bce5d17df9e9cacc859901022
hejl <hjlarry@163.com>
2022-10-07 19:02:16 +0800

    add load file example

--- StorageApplication/app/src/main/java/com/example/storageapplication/MainActivity.kt
+++ StorageApplication/app/src/main/java/com/example/storageapplication/MainActivity.kt
@@ -3,26 +3,40 @@ package com.example.storageapplication
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.widget.Toast
+import com.example.storageapplication.databinding.ActivityMainBinding
+import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.IOException
+import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.savebtn.setOnClickListener {
+            val text = binding.editText.text.toString()
+            save(text)
+        }
+
+        binding.loadbtn.setOnClickListener {
+            val inputText = load()
+            if (inputText.isNotEmpty()) {
+                binding.editText.setText(inputText)
+                binding.editText.setSelection(inputText.length)
+                Toast.makeText(this, "load success", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        val text = findViewById<EditText>(R.id.editText).text.toString()
-        save(text)
-    }
 
     private fun save(text: String) {
         try {
-            val output = openFileOutput("data", Context.MODE_PRIVATE)
+            val output = openFileOutput("mydata", Context.MODE_PRIVATE)
             val writer = BufferedWriter(OutputStreamWriter(output))
             writer.use {
                 it.write(text)
@@ -30,5 +44,21 @@ class MainActivity : AppCompatActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    private fun load(): String {
+        val content = StringBuilder()
+        try {
+            val input = openFileInput("mydata")
+            val reader = BufferedReader(InputStreamReader(input))
+            reader.use {
+                reader.forEachLine {
+                    content.append(it)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return content.toString()
     }
 }
