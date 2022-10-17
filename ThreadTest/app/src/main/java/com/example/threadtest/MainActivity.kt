2ff533baec963cfdb33dd48dcab02c862d49aa4a
hejl <hjlarry@163.com>
2022-10-17 14:44:29 +0800

    add service bind activity

--- ThreadTest/app/src/main/java/com/example/threadtest/MainActivity.kt
+++ ThreadTest/app/src/main/java/com/example/threadtest/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.example.threadtest
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import android.widget.Button
 import android.widget.TextView
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     val update_text = 1
+    lateinit var downloadBinder: MyService.VisualDownloadBinder
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            downloadBinder = service as MyService.VisualDownloadBinder
+            downloadBinder.startDownload()
+            downloadBinder.getProgress()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+    }
 
     val handler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
@@ -48,5 +59,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MyService::class.java)
             stopService(intent)
         }
+
+        val bindBtn = findViewById<Button>(R.id.bind)
+        bindBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+
+        val unbindBtn = findViewById<Button>(R.id.unbind)
+        unbindBtn.setOnClickListener {
+            unbindService(connection)
+        }
+
+
     }
 }
--- ThreadTest/app/src/main/java/com/example/threadtest/MyService.kt
+++ ThreadTest/app/src/main/java/com/example/threadtest/MyService.kt
@@ -2,11 +2,25 @@ package com.example.threadtest
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 
 class MyService : Service() {
     val tag = "My Service"
+    private val mBinder = VisualDownloadBinder()
+
+    inner class VisualDownloadBinder : Binder() {
+        fun startDownload() {
+            Log.d(tag, "start download()")
+        }
+
+        fun getProgress(): Int {
+            Log.d(tag, "get progress()")
+            return 0
+        }
+    }
+
     override fun onCreate() {
         Log.d(tag, "onCreate")
         super.onCreate()
@@ -23,6 +37,6 @@ class MyService : Service() {
     }
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return mBinder
     }
 }
