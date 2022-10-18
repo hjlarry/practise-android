a518edf93fe9b30f639232b04611e96d20fa1a2d
hejl <hjlarry@163.com>
2022-10-18 11:02:52 +0800

    add send request with okhttp

--- webtest/app/src/main/java/com/example/webtest/MainActivity.kt
+++ webtest/app/src/main/java/com/example/webtest/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.webtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.TextView
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -19,7 +19,8 @@ class MainActivity : AppCompatActivity() {
 
         val btn = findViewById<Button>(R.id.button)
         btn.setOnClickListener {
-            sendRequestWithHttpURLConnection()
+//            sendRequestWithHttpURLConnection()
+            sendRequestWithOkHttp()
         }
     }
 
@@ -44,6 +45,20 @@ class MainActivity : AppCompatActivity() {
                 e.printStackTrace()
             } finally {
                 connection?.disconnect()
+            }
+        }
+    }
+
+    private fun sendRequestWithOkHttp() {
+        thread {
+            try {
+                val client = OkHttpClient()
+                val request = Request.Builder().url("https://www.baidu.com").build()
+                val response = client.newCall(request).execute()
+                val responseData = response.body.string()
+                showResponse(responseData)
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
     }
