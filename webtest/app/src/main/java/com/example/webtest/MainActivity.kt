969d222b8315dbd5cbb2672b19c97161614805a2
hejl <hjlarry@163.com>
2022-10-18 10:50:39 +0800

    add send request

---
+++ webtest/app/src/main/java/com/example/webtest/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.webtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.TextView
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            sendRequestWithHttpURLConnection()
+        }
+    }
+
+    private fun sendRequestWithHttpURLConnection() {
+        thread {
+            var connection: HttpURLConnection? = null
+            try {
+                val response = StringBuilder()
+                val url = URL("https://www.baidu.com")
+                connection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+                val input = connection.inputStream
+                val reader = BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine {
+                        response.append(it)
+                    }
+                }
+                showResponse(response.toString())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+        }
+    }
+
+    private fun showResponse(res: String) {
+        runOnUiThread {
+            val resView = findViewById<TextView>(R.id.resText)
+            resView.text = res
+        }
+    }
+}
