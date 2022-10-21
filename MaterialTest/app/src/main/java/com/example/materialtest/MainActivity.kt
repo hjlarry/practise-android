05c3001fd5a20777f48983a0473113d71761faf8
hejl <hjlarry@163.com>
2022-10-21 11:44:42 +0800

    add drawerlayout

--- MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.kt
+++ MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.kt
@@ -3,14 +3,29 @@ package com.example.materialtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import androidx.appcompat.widget.Toolbar
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import com.example.materialtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toobar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toobar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
