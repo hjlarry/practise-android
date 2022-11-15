7d86b454e36688c75346915ae8bbe17dcaa614b0
hejl <hjlarry@163.com>
2022-11-15 09:50:37 +0800

    add enum class

--- LearnKotlin/app/src/main/java/com/example/learnkotlin/SpecialClasses.kt
+++ LearnKotlin/app/src/main/java/com/example/learnkotlin/SpecialClasses.kt
@@ -1,5 +1,6 @@
 package com.example.learnkotlin
 
+
 /*
     数据类是创建只包含数据的类的一个简洁的方法。
     "hashCode"、"equals"和"toString"方法将自动生成。
@@ -8,7 +9,7 @@ data class User(val name: String, val id: Int) {
     override fun equals(other: Any?) = other is User && other.id == this.id
 }
 
-fun testDataclass() {
+fun testDataClass() {
     val user = User("Alex", 1)
     println(user)
 
@@ -33,7 +34,38 @@ fun testDataclass() {
     println("id = ${user.component2()}")
 }
 
+/*
+    枚举类
+*/
+enum class State {
+    IDLE, RUNNING, FINISHED
+}
+enum class Color(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF),
+    YELLOW(0xFFFF00);
+
+    fun containsRed() = (this.rgb and 0xFF0000 != 0)
+}
+
+fun testEnumClass() {
+    val state = State.RUNNING
+    val message = when (state) {
+        State.IDLE -> "It's idle"
+        State.RUNNING -> "It's running"
+        State.FINISHED -> "It's finished"
+    }
+    println(message)
+
+    val red = Color.RED
+    println(red)
+    println(red.rgb)
+    println(red.containsRed())
+    println(Color.BLUE.containsRed())
+}
 
 fun main() {
-    testDataclass()
+    testDataClass()
+    testEnumClass()
 }
