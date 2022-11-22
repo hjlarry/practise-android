4cf21637d1e6b596c942c65cf6b2e96f1afa00a2
hejl <hjlarry@163.com>
2022-11-22 16:28:32 +0800

    scheduling work

--- ToDo/app/src/main/java/com/commonsware/todo/ToDoApp.kt
+++ ToDo/app/src/main/java/com/commonsware/todo/ToDoApp.kt
@@ -2,10 +2,8 @@ package com.commonsware.todo
 
 import android.app.Application
 import android.text.format.DateUtils
-import com.commonsware.todo.repo.PrefsRepository
-import com.commonsware.todo.repo.ToDoDatabase
-import com.commonsware.todo.repo.ToDoRemoteDateSource
-import com.commonsware.todo.repo.ToDoRepository
+import androidx.work.*
+import com.commonsware.todo.repo.*
 import com.commonsware.todo.report.RosterReport
 import com.commonsware.todo.ui.SingleModelMotor
 import com.commonsware.todo.ui.roster.RosterMotor
@@ -13,17 +11,24 @@ import com.github.jknack.handlebars.Handlebars
 import com.github.jknack.handlebars.Helper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import java.time.Instant
+import java.util.concurrent.TimeUnit
 
-class ToDoApp : Application() {
+private const val TAG_IMPORT_WORK = "doPeriodicImport"
+
+class ToDoApp : Application(), KoinComponent {
     private val koinModule = module {
         single { ToDoRepository(get<ToDoDatabase>().todoStore(), get(named("appScope")), get()) }
         viewModel { RosterMotor(get(), get(), androidApplication(), get(named("appScope")), get()) }
@@ -49,6 +54,32 @@ class ToDoApp : Application() {
         single { PrefsRepository(androidContext()) }
     }
 
+    private fun scheduleWork() {
+        val prefs: PrefsRepository by inject()
+        val appScope: CoroutineScope by inject(named("appScope"))
+        val workManager = WorkManager.getInstance(this)
+        appScope.launch {
+            prefs.observeImportChanges().collect {
+                if (it) {
+                    val constraints = Constraints.Builder()
+                        .setRequiredNetworkType(NetworkType.CONNECTED)
+                        .build()
+                    val request = PeriodicWorkRequestBuilder<ImportWorker>(15, TimeUnit.MINUTES)
+                        .setConstraints(constraints)
+                        .addTag(TAG_IMPORT_WORK)
+                        .build()
+                    workManager.enqueueUniquePeriodicWork(
+                        TAG_IMPORT_WORK,
+                        ExistingPeriodicWorkPolicy.REPLACE,
+                        request
+                    )
+                } else {
+                    workManager.cancelAllWorkByTag(TAG_IMPORT_WORK)
+                }
+            }
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -56,5 +87,6 @@ class ToDoApp : Application() {
             androidContext(this@ToDoApp)
             modules(koinModule)
         }
+        scheduleWork()
     }
 }
--- ToDo/app/src/main/java/com/commonsware/todo/repo/PrefsRepository.kt
+++ ToDo/app/src/main/java/com/commonsware/todo/repo/PrefsRepository.kt
@@ -1,18 +1,33 @@
 package com.commonsware.todo.repo
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import com.commonsware.todo.R
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.withContext
 
 class PrefsRepository(context: Context) {
     private val prefs = PreferenceManager.getDefaultSharedPreferences(context)
     private val webServiceUrlKey = context.getString(R.string.web_service_url_key)
     private val defaultWebServiceUrl = context.getString(R.string.web_service_url_default)
+    private val importKey = context.getString(R.string.import_key)
 
     suspend fun loadWebServiceUrl() = withContext(Dispatchers.IO) {
         prefs.getString(webServiceUrlKey, defaultWebServiceUrl) ?: defaultWebServiceUrl
+    }
+
+    fun observeImportChanges() = channelFlow {
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+            if (importKey == key) {
+                offer(prefs.getBoolean(importKey, false))
+            }
+        }
+
+        prefs.registerOnSharedPreferenceChangeListener(listener)
+        awaitClose { prefs.unregisterOnSharedPreferenceChangeListener(listener) }
     }
 
 }
